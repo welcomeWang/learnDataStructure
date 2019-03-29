0aab7c5540ebe004d472865a7350b716763a4444
welcomeWang <apricotw@foxmail.com>
2019-03-30 04:17:49 +0800

    Helloworld test

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 16283 on 2019/3/30.
+//
+#include <iostream>
+using namespace std;
+int main(void){
+    cout << "HelloWorld!"<< endl;
+}
+
---
+++ cmake-build-debug/CMakeFiles/learnDataStructure.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/learnDataStructure.dir/HelloWorld.cpp.obj"
+  "learnDataStructure.pdb"
+  "learnDataStructure.exe"
+  "learnDataStructure.exe.manifest"
+  "liblearnDataStructure.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/learnDataStructure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
